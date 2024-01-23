b55449d8804fe523dda77aa6dc111da8d44a8bf6
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2024-01-23 14:40:16 -0300

    Add route for getting a single event

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import * as events from '../controllers/events';
 
 const router = Router();
 
 router.get('/ping', (req, res) => res.json({ pong: true }));
+
+router.get('/events/:id', events.getOne);
 
 export const siteRoutes = router;
